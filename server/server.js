1b13db1515fbc9a1f5cca761badd7875cb00cb1d
SG_Demi <stevengoldbergm@gmail.com>
2022-10-21 23:27:48 -0500

    Added endpoint / to the server

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const db = require('./config/connection');
 // Remove the routes
   // const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 // Add in the server
 const server = new ApolloServer({
   typeDefs,
@@ -18,13 +18,17 @@ const server = new ApolloServer({
   context: authMiddleware,
 })
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Remove routes
   // app.use(routes);
@@ -37,7 +41,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   db.once('open', () => {
     app.listen(PORT, () => {
-      console.log(`🌍 Now listening on localhost:${PORT}`);
+      console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
     })
   })
