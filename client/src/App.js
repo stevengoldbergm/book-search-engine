0f929e2ea966cd09893f3ef95b6e5656acbdee6e
SG_Demi <stevengoldbergm@gmail.com>
2022-10-21 20:14:29 -0500

    Added resolver logic to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { 
+  ApolloClient, 
+  ApolloProvider, 
+  InMemoryCache, 
+  createHttpLink 
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
