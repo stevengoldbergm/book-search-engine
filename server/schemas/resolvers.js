c3789833b00c537df3a865c9b52234f4effa5700
SG_Demi <stevengoldbergm@gmail.com>
2022-10-21 19:02:44 -0500

    Added resolvers based on user-controller queries

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,53 @@
+const {AuthenticationError} = require('apollo-server-express');
+
+const { User } = require('../models');
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+    Query: {
+        getUser: async ( parent, { user = null, args } ) => {
+            return await User.findOne({
+                $or: [
+                    { _id: user ? user._id : args.id }, 
+                    { username: args.username }
+                ],
+              });
+        },
+    },
+
+    
+    Mutations: {
+        createUser: async ( parent, { body } ) => {
+            const user = await User.create(body);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async ( parent, { body }) => {
+            const user = await User.findOne({ 
+                $or: [
+                    { username: body.username }, 
+                    { email: body.email }
+                ] 
+            });
+            const correctPw = await user.isCorrectPassword(body.password);
+            const token = signToken(user);
+            return {token, user};
+        },
+        saveBook: async (parent, { user, body }) => {
+            return await User.findOneAndUpdate(
+                { _id: user._id },
+                { $addToSet: { savedBooks: body } },
+                { new: true, runValidators: true }
+            );
+        },
+        deleteBook: async ( parent, {user, args} ) => {
+            return await User.findOneAndUpdate(
+                { _id: user._id },
+                { $pull: { savedBooks: { bookId: params.bookId } } },
+                { new: true }
+              );
+        },
+    },
+};
+
+module.exports = resolvers;
